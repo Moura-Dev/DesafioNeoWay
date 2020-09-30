017389a7dbe5b9e4b39568727eb3055391fb7810
Moura-Dev <junior.moura19@hotmail.com>
2020-09-29 23:50:26 -0300

    Recuperandos valores

---
+++ main.py
@@ -0,0 +1,22 @@
+from os import name
+import requests
+from bs4 import BeautifulSoup
+from pprint import pprint
+
+
+payload = {'UF':'SP','qtdrow':'100','pagini':'1','pagfim':'100'}
+url = 'http://www.buscacep.correios.com.br/sistemas/buscacep/resultadoBuscaFaixaCEP.cfm'
+
+#css_soup = BeautifulSoup('<p class="body"></p>')
+#css_soup.p['class']
+
+htmlResponse = requests.post(url,data=payload).content
+soup = BeautifulSoup(htmlResponse, 'html.parser')
+makediv = soup.find_all(["td", "class='tmptabela"])
+for tag in makediv:
+    todascidades= []
+    cidade = ""
+    cidades = [tag.text]
+    for item in tag:
+        todascidades.append(item)
+        print(item)
---
+++ teste.py
@@ -0,0 +1,23 @@
+from os import name
+import requests
+from bs4 import BeautifulSoup
+from pprint import pprint
+
+
+payload = {'UF':'SP','qtdrow':'100','pagini':'1','pagfim':'100'}
+url = 'http://www.buscacep.correios.com.br/sistemas/buscacep/resultadoBuscaFaixaCEP.cfm'
+
+#css_soup = BeautifulSoup('<p class="body"></p>')
+#css_soup.p['class']
+
+htmlResponse = requests.post(url,data=payload).content
+soup = BeautifulSoup(htmlResponse, 'html.parser')
+makediv = soup.find_all(["td", "class='tmptabela"])
+for tag in makediv:
+    cidade = ""
+    cidade = tag.text
+    print(cidade)
+
+
+
+#print(makediv)
