91c44e2f773829940baa769eb5d1b5492922bf61
Moura-Dev <junior.moura19@hotmail.com>
2020-10-01 21:19:37 -0300

    Removendo Acento no Json

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ from typing import ValuesView
 import requests
 from bs4 import BeautifulSoup
 from pprint import pprint
+import unicodedata
+import unidecode
 
 cidade = "SP"
 payload = {'UF':cidade,'qtdrow':'100','pagini':'1','pagfim':'100'}
@@ -12,15 +14,17 @@ url = 'http://www.buscacep.correios.com.br/sistemas/buscacep/resultadoBuscaFaixa
 
 htmlResponse = requests.post(url,data=payload).content.decode('iso-8859-1')
 soup = BeautifulSoup(htmlResponse, 'html.parser')
-makediv = soup.find_all(["td", 'class=tmptabela"'])
+makediv = soup.find_all(["td", 'class=tmptabela'])
 todascidades= []
 todasUF = {}
+
 
 for tag in makediv:
     for item in tag:
         if item == "Não codificada por logradouros" or item == "Total do município" or item=="Codificado por logradouros" or item=="Codificada por logradouros" or item=="Total do município" or item=="Exclusiva da sede urbana":
             pass
         else:
+            item = unidecode.unidecode(item)
             todascidades.append(item.strip())
 
 todasUF = {todascidades[i]: todascidades[i + 1] for i in range(0, len(todascidades), 2)}
