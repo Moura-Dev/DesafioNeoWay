83e164c92fe0f67b3e1597c344addbd014bac9b8
Moura-Dev <junior.moura19@hotmail.com>
2020-10-04 18:41:28 -0300

    Organização do codigo

--- main.py
+++ main.py
@@ -5,10 +5,7 @@ import requests
 from bs4 import BeautifulSoup
 from pprint import pprint
 import unidecode
-listauf = ["AL","BA","CE","GO","MA","MG","MT","PA","PB","PE","PR","RJ","RN","RS","SC","SP","TO"]
-#uf = "PA"
-#payload = {'UF':uf,'qtdrow':'100','pagini':'1','pagfim':'100'}
-#payload2 = {'UF':uf,'qtdrow':'100','pagini':'101','pagfim':'200'}
+listauf = ["AL","BA","CE","GO","MA","MG","MT","PA","PB","PE","PR","RJ","RS","SC","SP",]
 url = 'http://www.buscacep.correios.com.br/sistemas/buscacep/resultadoBuscaFaixaCEP.cfm'
 todascidades= []
 todascidades2=[]
@@ -16,6 +13,12 @@ todascidadesConcatenadas = []
 todasUF = {}
 todasUF2 = {}
 todasUFConcatenadas = {}
+ignorar = (
+    "Não codificada por logradouros", "Total do município",
+    "Codificado por logradouros", "Codificada por logradouros",
+    "Total do município", "Exclusiva da sede urbana")
+
+
 #funcao principal
 def main(uf):
     payload = {'UF':uf,'qtdrow':'100','pagini':'1','pagfim':'100'}
@@ -31,7 +34,6 @@ def makeSoup(url,payload):
     todascidades= []
     todascidadesConcatenadas = []
     todasUF = {}
- 
     todasUFConcatenadas = {}
     htmlResponse = requests.post(url,data=payload).content.decode('iso-8859-1')
     soup = BeautifulSoup(htmlResponse, 'html.parser')
@@ -39,9 +41,7 @@ def makeSoup(url,payload):
        
     for tag in makediv:
         for item in tag:
-            if item == "Não codificada por logradouros" or item == "Total do município" or item=="Codificado por logradouros" or item=="Codificada por logradouros" or item=="Total do município" or item=="Exclusiva da sede urbana":
-                pass
-            else:
+            if item not in ignorar:
                 item = unidecode.unidecode(item)
                 todascidades.append(item.strip())
 
@@ -51,20 +51,16 @@ def makeSoup(url,payload):
 
 #montar lista de 101 a 200
 def makeSoup2(url,payload2):
-
     todascidades2=[]
     todasUF2 = {}
     htmlResponse = requests.post(url,data=payload2).content.decode('iso-8859-1')
     soup = BeautifulSoup(htmlResponse, 'html.parser')
     makediv = soup.find_all(["td", 'class=tmptabela'])
-
     for tag in makediv:
         for item in tag:
-            if item == "Não codificada por logradouros" or item == "Total do município" or item=="Codificado por logradouros" or item=="Codificada por logradouros" or item=="Total do município" or item=="Exclusiva da sede urbana":
-                pass
-            else:
-                item = unidecode.unidecode(item)
-                todascidades2.append(item.strip())
+                if item not in ignorar:
+                    item = unidecode.unidecode(item)
+                    todascidades.append(item.strip())
 
     todasUF2 = {todascidades2[i]: todascidades2[i + 1] for i in range(0, len(todascidades2), 2)}
     return todascidades2
--- webscraping.py
+++ webscraping.py
@@ -1,95 +1,48 @@
-from os import name
 import json
-from typing import ValuesView
+import re
 import requests
 from bs4 import BeautifulSoup
-from pprint import pprint
-import unidecode
-listauf = ["AL","BA","CE","GO","MA","MG","MT","PA","PB","PE","PR","RJ","RN","RS","SC","SP","TO"]
-#uf = "PA"
-#payload = {'UF':uf,'qtdrow':'100','pagini':'1','pagfim':'100'}
-#payload2 = {'UF':uf,'qtdrow':'100','pagini':'101','pagfim':'200'}
+
 url = 'http://www.buscacep.correios.com.br/sistemas/buscacep/resultadoBuscaFaixaCEP.cfm'
-todascidades= []
-todascidades2=[]
-todascidadesConcatenadas = []
-todasUF = {}
-todasUF2 = {}
-todasUFConcatenadas = {}
-#funcao principal
-def main(uf):
-    payload = {'UF':uf,'qtdrow':'100','pagini':'1','pagfim':'100'}
-    payload2 = {'UF':uf,'qtdrow':'100','pagini':'101','pagfim':'200'}
-    soup = makeSoup(url,payload)
-    soup2 = makeSoup2(url,payload2)
-    merge = mergeDict(soup,soup2)
-    DumpJson(merge)
+UFs = (
+    "AC","AL","AM","AP","BA","CE","DF","ES","GO","MA","MG","MS","MT","PA","PB","PE","PI",
+    "PR","RJ","RN","RO","RR","RS","SC","SE","SP","TO"
+    )
+inícios = (1, 100)
+fins = (100, 200)
+cidade = []
+cidades = {}
+
+ignorar = (
+    "Não codificada por logradouros", "Total do município",
+    "Codificado por logradouros", "Codificada por logradouros",
+    "Total do município", "Exclusiva da sede urbana"
+    )
 
 
-#montar lista de 1 a 100
-def makeSoup(url,payload):
-    todascidades= []
-    todascidadesConcatenadas = []
-    todasUF = {}
- 
-    todasUFConcatenadas = {}
-    htmlResponse = requests.post(url,data=payload).content.decode('iso-8859-1')
+def makeSoup(url, UF, inifim):
+    payload = {'UF': UF, 'pagini': inifim[0], 'pagfim': inifim[1]}
+    htmlResponse = requests.post(url, data=payload).content
     soup = BeautifulSoup(htmlResponse, 'html.parser')
-    makediv = soup.find_all(["td", 'class=tmptabela'])
-       
-    for tag in makediv:
-        for item in tag:
-            if item == "Não codificada por logradouros" or item == "Total do município" or item=="Codificado por logradouros" or item=="Codificada por logradouros" or item=="Total do município" or item=="Exclusiva da sede urbana":
-                pass
-            else:
-                item = unidecode.unidecode(item)
-                todascidades.append(item.strip())
-
-    todasUF = {todascidades[i]: todascidades[i + 1] for i in range(0, len(todascidades), 2)}
-    return todascidades
+    if not soup.find_all(string=re.compile("developer")):
+        # if soup.findAll(text="developer"):
+        make = soup.find_all(["td"])
+        for tds in make:
+            for td in tds:
+                if td not in ignorar:
+                    cidade.append(td.strip())
 
 
-#montar lista de 101 a 200
-def makeSoup2(url,payload2):
-
-    todascidades2=[]
-    todasUF2 = {}
-    htmlResponse = requests.post(url,data=payload2).content.decode('iso-8859-1')
-    soup = BeautifulSoup(htmlResponse, 'html.parser')
-    makediv = soup.find_all(["td", 'class=tmptabela'])
-
-    for tag in makediv:
-        for item in tag:
-            if item == "Não codificada por logradouros" or item == "Total do município" or item=="Codificado por logradouros" or item=="Codificada por logradouros" or item=="Total do município" or item=="Exclusiva da sede urbana":
-                pass
-            else:
-                item = unidecode.unidecode(item)
-                todascidades2.append(item.strip())
-
-    todasUF2 = {todascidades2[i]: todascidades2[i + 1] for i in range(0, len(todascidades2), 2)}
-    return todascidades2
+def DumpJson(cidade, cidades, UF):
+    cidades = {cidade[i]: cidade[i + 1] for i in range(0, len(cidade), 2)}
+    with open(UF+'.json', 'w', encoding="utf-8") as jp:
+        js = json.dumps(cidades, ensure_ascii=False)
+        jp.write(js)
 
 
-#Mesclar dicionários e adicionar valores de chaves comuns em uma lista
-def mergeDict(todascidades,todascidades2):
-
-    for d in todascidades2:
-        todascidades.append(d)
-    todascidadesConcatenadas = todascidades
-    todasUFConcatenadas = {todascidadesConcatenadas[i]: todascidadesConcatenadas[i + 1] for i in range(0, len(todascidadesConcatenadas), 2)}
-    return todasUFConcatenadas
-
-
-#Converter json e criar arquivo.json
-def DumpJson(todasUFConcatenadas):
-    documento = json.dumps(todasUFConcatenadas)
-    arquivo = open(uf+'.json', 'w')
-    arquivo.write(documento)
-    arquivo.close()
-    print("UF Gravado com Sucesso!")
-
-
-for k in listauf:
-    uf = k
-    main(uf)
-
+for UF in UFs:
+    for inifim in inícios, fins:
+        makeSoup(url, UF, inifim)
+    DumpJson(cidade, cidades, UF)
+    cidade = []
+    cidades.clear
